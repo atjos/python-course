fcc493025a2a04d31631b58b6c8962a4f1cbb5ce
atjos <juanjomen2za@gmail.com>
2025-10-11 14:15:13 -0500

    empezar de 0

--- Python/tipos/03-format-strings.py
+++
@@ -1,5 +0,0 @@
-nombre = "Juan José"
-apellido = "Mendoza"
-edad = 18
-nombre_completo = f"{nombre} {apellido} {edad}"
-print(nombre_completo)
--- Python/tipos/04-metodos-strings.py
+++
@@ -1,8 +0,0 @@
-comida = "fresas con crema"
-print(comida.upper())  # Convierte a mayúsculas
-print(comida.lower())  # Convierte a minúsculas
-print(comida.capitalize())  # Convierte la primera letra a mayúscula
-print(comida.title())  # Convierte la primera letra de cada palabra a mayúscula
-print(comida.find("s")) # Busca la posición de la primera "s"
-print(comida.replace("crema", "chocolate")) # Reemplaza "crema" por "chocolate"
-print("con" in comida)  # Verifica si "con" está en la cadena
