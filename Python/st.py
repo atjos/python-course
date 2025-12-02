657becc51151da27dc09a10da444444c690756a5
atjos <juanjomen2za@gmail.com>
2025-12-01 20:08:50 -0500

    tuto 12 dic

--- Python/st.py
+++ Python/st.py
@@ -2,3 +2,17 @@
 print("Hola Mundo!!!")
 print("Este es mi primer programa en Python " * 4)
 
+
+#Consultar el tipo de dato
+
+print(type(5)) # Tipo 'int'
+
+"""
+Comentarios en Python
+para varias líneas
+"""
+
+print("Juan José Mendoza, Colombia, I am enjoying 30 days of Python")
+print(type(10))
+print(type(9.8))
+print(type(4-4j))
--- Python/tipos/01-variables.py
+++
@@ -1,6 +0,0 @@
-nombre_curso = "Ultimate Python"
-print(nombre_curso)
-
-alumnos = 5000
-profesores = 100
-print(alumnos, profesores)
