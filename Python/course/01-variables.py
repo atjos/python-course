d188643375d51b2bcd1f2c66daa8386e248368e3
atjos <juanjomen2za@gmail.com>
2026-01-21 00:10:28 -0500

    tuto 21 ene

--- Python/course/01-variables.py
+++ Python/course/01-variables.py
@@ -3,3 +3,32 @@
 NoMbre = "Juan José"
 print(NoMbre)
 
+#Input
+
+name = input("Enter your first name: ")
+
+resultado = "Your first name is " + name
+
+print(resultado)
+
+#Exercises - Day 2: 30 days of Python programming (Level 1)
+
+first_name = "Juan"
+last_name = "Mendoza"
+full_name = "Juan José Mendoza Caballero"
+country = "Colombia"
+city = "Cartagena"
+age = 18
+birth_year = 2007
+is_married = False
+is_light_on = True
+
+pets, car, bike = "Dog", "Jeep", "Yamaha"
+
+#Exercises - Day 2: 30 days of Python programming (Level 2)
+
+print(type(first_name))
+print(len(last_name))
+print(len(first_name) > len(last_name))
+
+
