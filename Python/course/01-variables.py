a965905762cc3b520566919a6e7117243efa060b
atjos <juanjomen2za@gmail.com>
2026-07-24 23:20:46 -0500

    tuto 24 jul

--- Python/course/01-variables.py
+++ Python/course/01-variables.py
@@ -3,6 +3,20 @@
 NoMbre = "Juan José"
 print(NoMbre)
 
+#Tipos de datos
+
+a, b, c = 5, 3.2, 1+2j #Enteros, flotantes, complejos (int, float, complex)
+
+d, e = True, False #Booleanos (bool)
+
+f, g = "Hola", 'M' #Cadenas de texto (str)
+
+h, i = [1, 2, 3], ["perro", "gato", "loro"] #Listas (list)
+
+j, k = (4, 5), ("azul", "rojo", "verde") #Tuplas (tuple)
+
+l = None #Nulo (NoneType)
+
 #Input
 
 name = input("Enter your first name: ")
@@ -11,6 +25,9 @@ resultado = "Your first name is " + name
 
 print(resultado)
 
+
+
+
 #Exercises - Day 2: 30 days of Python programming (Level 1)
 
 first_name = "Juan"
@@ -18,7 +35,7 @@ last_name = "Mendoza"
 full_name = "Juan José Mendoza Caballero"
 country = "Colombia"
 city = "Cartagena"
-age = 18
+age = 8
 birth_year = 2007
 is_married = False
 is_light_on = True
@@ -31,4 +48,11 @@ print(type(first_name))
 print(len(last_name))
 print(len(first_name) > len(last_name))
 
+y = 10
+y = "hola"
+
+print(y)
+
+age = input("Enter your age: ")
+resultado_edad = print("Your age is " + age)
 
---
+++ Python/course/02-operadores.py
@@ -0,0 +1,76 @@
+### Operadores Aritméticos###
+
+print(5 + 5)
+print(5 - 12)
+print(5 * 10)
+print(5 / 8)
+print(10 % 3)  # Módulo - Resto de una división
+print(2 ** 3)
+print(10 // 3)  # División entera - Parte entera de una división
+
+float = 3.7 * 2
+# Repetición de cadenas usando la función int() para convertir float a entero
+print("Hola " * int(float))
+
+# Operadores de comparación
+
+print(8 > 3)
+print(5 < 2)
+print(10 >= 10)
+print(4 <= 1)
+print(7 == 7)
+print(3 != 5)
+
+print("Hola" > "Python")
+
+edad_usuario = 19
+verificación_edad = 20
+acceso = edad_usuario >= verificación_edad
+print("¿El usuario tiene acceso?", acceso)
+
+# Calculadora
+
+price = 2000
+products = 3
+total = price * products
+iva = total * 0.19
+final_price = total + iva
+print("El precio total es:", final_price)
+
+# Operadores Lógicos
+
+age = 18
+has_license = True
+
+if age > 0 and has_license is True:
+    print("Está apto para conducir")
+else:
+    print("No está apto para conducir")
+
+# Queremos verificar si una persona puede conducir un vehículo en la ciudad de Bucaramanga
+
+age = 19
+has_license = True
+borracho = False
+
+if age > 18 and has_license is True and borracho is False:
+    print("Puede conducir")
+else:
+    print("No puede conducir el vehículo")
+
+# Operadores de asignación
+
+primer_producto = 40000
+segundo_producto = 35000
+tercer_producto = 40000
+
+if primer_producto >= 50000 and segundo_producto >= 50000 and tercer_producto <= 50000:
+    primer_producto *= 0.7
+    segundo_producto *= 0.7
+    print("El total de su compra es:", primer_producto + segundo_producto)
+else:
+    print("El total de su compra es:", primer_producto +
+        segundo_producto + tercer_producto)
+
+
+
