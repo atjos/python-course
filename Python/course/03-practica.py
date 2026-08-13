58d444ba236f3db122f134786528ad663dd2cb19
atjos <juanjomen2za@gmail.com>
2026-08-12 20:43:28 -0500

    finalizado strings

--- Python/course/03-practica.py
+++ Python/course/03-practica.py
@@ -1,4 +1,7 @@
 company = "Coding For All"
+companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+frase = "You cannot end a sentence with because because because is a conjunction"
+
 print(len(company))
 
 print(company.upper())
@@ -14,3 +17,23 @@ print(company.swapcase())
 print(company.find("Coding"))
 
 print(company.replace("Coding", "Python"))
+
+print(company.split())
+
+print(companies.split(','))
+
+print(company.index("F"))
+
+print(company.rindex("l"))
+
+print(frase.find("because"))
+
+print(frase.rindex("because"))
+
+print(frase[31:47])
+
+print(company.startswith("Coding"))
+
+radius = 10
+area = 3.14 * radius ** 2
+print(f"El area del círculo con radio {radius} es: {area}")
